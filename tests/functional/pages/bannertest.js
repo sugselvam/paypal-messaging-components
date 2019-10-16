99794d5ac74fbfc2f43009dc255d3af1df715953
Suganya Selvam <sugselvam@paypal.com>
2019-10-16 18:48:32 +0530

    dev server changes for banner.html

--- tests/functional/pages/bannertest.js
+++ tests/functional/pages/bannertest.js
@@ -1,12 +1,12 @@
 const selectors = {
-    ezpayopen: "xpath://span/span[text()='Pay over 3 months']"
+    ezpayopen: "xpath://div[@id='EZ']"
 };
 
 exports.entrypage = function(nemo) {
     return {
         async viewezpaybanner() {
             await nemo.view._waitVisible(selectors.ezpayopen);
-            await nemo.view._find(selectors.ezpay).click();
+            await nemo.view._find(selectors.ezpayopen).click();
         },
         async collectiveEntry() {
             await this.viewezpaybanner();
