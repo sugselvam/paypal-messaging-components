10abea35d9ce5c2004b3d86aa44ca3095c942459
Suganya Selvam <sugselvam@paypal.com>
2019-10-18 17:40:21 +0530

    updated testcases

--- tests/functional/pages/bannertest.js
+++ tests/functional/pages/bannertest.js
@@ -1,6 +1,8 @@
 const selectors = {
-    ezpayIframe: 'xpath:/html/body/div[1]/div[1]/span/iframe',
-    ezpayopen: "xpath://div[@class= 'message__content']"
+    ezpayopen: "xpath://div[@class= 'message__content']",
+    ezpayclose: "xpath:/html/body/section[@class='modal__container show']/div/div/div[1]/div/div/header/button",
+    NIopen: "xpath://div[@class= 'message__content']",
+    textbox: '#number-input'
 };
 
 exports.entrypage = function(nemo) {
@@ -11,8 +13,22 @@ exports.entrypage = function(nemo) {
             await nemo.view._find(selectors.ezpayopen).click();
             await nemo.driver.sleep(5000);
         },
+        /* async closeezpaybanner()
+        {   
+            await nemo.view._waitVisible(selectors.ezpayclose);
+            await nemo.view._find(selectors.ezpayclose).click();
+            await nemo.driver.sleep(5000);
+        }, */
+        async viewNIbanner() {
+            await nemo.driver.switchTo().frame(1);
+            await nemo.view._waitVisible(selectors.NIopen);
+            await nemo.view._find(selectors.NIopen).click();
+            await nemo.driver.sleep(5000);
+        },
         async collectiveEntry() {
             await this.viewezpaybanner();
+            await this.closeezpaybanner();
+            await this.viewNIbanner();
         }
     };
 };
