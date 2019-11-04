eee4359127b1e9f56a4ecee0310481a1acfcf33a
Suganya Selvam <sugselvam@paypal.com>
2019-11-04 15:22:40 +0530

    testcase

--- tests/functional/pages/EZpaybannertest.js
+++ tests/functional/pages/EZpaybannertest.js
@@ -1,8 +1,9 @@
 const selectors = {
     ezpayopen: '.message__content',
-    // ezpayclose: 'xpath://html/body/section[@data-pp-modal="ezp"]/div/div/div[1]/div/div/header/button',
-    ezcloseban: '.close',
-    iframe: 'xpath://div[contains(@style,"display: block")]//iframe'
+    ezcloseban: '#close-btn',
+    iframe: 'xpath://div[contains(@style,"display: block")]//iframe',
+    ezpaytextbox: '.calculator__input',
+    ezpaycalculate: '.calculator__btn'
 };
 
 exports.ezpayentrypage = function(nemo) {
@@ -13,20 +14,56 @@ exports.ezpayentrypage = function(nemo) {
             await nemo.view._find(selectors.ezpayopen).click();
             await nemo.driver.sleep(5000);
             await nemo.driver.switchTo().defaultContent();
-            const iframe = await nemo.view._finds('xpath://iframe');
-            await console.log('iframe', iframe.length);
+        },
+        async calculateamount() {
+            const iframeElement = await nemo.view._finds('xpath://div[contains(@style,"display: block")]//iframe');
+            await nemo.driver.switchTo().frame(iframeElement[0]);
+            await nemo.view._waitVisible(selectors.ezpaytextbox);
+            await nemo.view._find(selectors.ezpaytextbox).sendKeys(300);
+            await nemo.view._find(selectors.ezpaycalculate).click();
+            await nemo.driver.sleep(5000);
+            await nemo.view._find(selectors.ezpaytextbox).clear();
+            await nemo.view._find(selectors.ezpaycalculate).click();
+            await nemo.driver.switchTo().defaultContent();
+            await nemo.driver.sleep(5000);
+        },
+        async closesOnEscKey() {
+            const iframeElement = await nemo.view._finds('xpath://div[contains(@style,"display: block")]//iframe');
+            await nemo.driver.switchTo().frame(iframeElement[0]);
+            await nemo.driver
+                .actions()
+                .sendKeys(nemo.wd.Key.ESCAPE)
+                .perform();
+            await nemo.driver.switchTo().defaultContent();
+            await nemo.driver.sleep(5000);
+        },
+        async closesOnOverlayClick() {
+            await this.viewezpaybanner();
+            const iframeElement = await nemo.view._finds('xpath://div[contains(@style,"display: block")]//iframe');
+            await nemo.driver.switchTo().frame(iframeElement[0]);
+            const container = await nemo.view._finds('xpath://section[@id="modal-container"]');
+            await nemo.driver
+                .actions()
+                .mouseMove(container[0], { x: 5, y: 5 })
+                .click()
+                .perform();
+            await nemo.driver.switchTo().defaultContent();
+            await nemo.driver.sleep(5000);
         },
         async closeezpaybanner() {
+            await this.viewezpaybanner();
             const iframeElement = await nemo.view._finds('xpath://div[contains(@style,"display: block")]//iframe');
-            await console.log('divframe', iframeElement.length);
-            await nemo.view._waitVisible(selectors.iframe);
             await nemo.driver.switchTo().frame(iframeElement[0]);
-            // await nemo.view._waitVisible(selectors.ezcloseban);
-            // await nemo.view._find(selectors.ezcloseban).click();
+            await nemo.view._waitVisible(selectors.ezcloseban);
+            await nemo.view._find(selectors.ezcloseban).click();
+            await nemo.driver.switchTo().defaultContent();
             await nemo.driver.sleep(5000);
         },
         async ezpaycollectiveEntry() {
             await this.viewezpaybanner();
+            await this.calculateamount();
+            await this.closesOnEscKey();
+            await this.closesOnOverlayClick();
             await this.closeezpaybanner();
         }
     };
--- tests/functional/pages/NIbannertest.js
+++ tests/functional/pages/NIbannertest.js
@@ -1,6 +1,7 @@
 const selectors = {
     NIopen: '.message__content',
-    NIclose: '#close-btn'
+    NIclose: '#close-btn',
+    iframe: 'xpath://div[contains(@style,"display: block")]//iframe'
 };
 
 exports.NIentrypage = function(nemo) {
@@ -9,9 +10,14 @@ exports.NIentrypage = function(nemo) {
             await nemo.driver.switchTo().frame(1);
             await nemo.view._waitVisible(selectors.NIopen);
             await nemo.view._find(selectors.NIopen).click();
+            await nemo.driver.switchTo().defaultContent();
             await nemo.driver.sleep(5000);
         },
         async closeNIbanner() {
+            const iframeElement = await nemo.view._finds('xpath://div[contains(@style,"display: block")]//iframe');
+            await console.log('divframe', iframeElement.length);
+            await nemo.view._waitVisible(selectors.iframe);
+            await nemo.driver.switchTo().frame(iframeElement[0]);
             await nemo.view._waitVisible(selectors.NIclose);
             await nemo.view._find(selectors.NIclose).click();
             await nemo.driver.sleep(5000);
